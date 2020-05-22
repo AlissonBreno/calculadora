0e25bb88e0272702c49d3255ca684ebd23020578
Alisson Goulart <alissonbrenoemp@gmail.com>
2020-05-22 16:50:25 -0300

    starting memory

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,13 +1,27 @@
 class Memory{
+  static const operations = const ['%', '/', 'X', '-', '+', '='];
+  
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  String operation;
   String _value = '0';
 
   void applyCommand(String command){
     if(command == 'AC'){
       allClear();
+    } else if(operations.contains(command)){
+      _setOperation(command);
     } else{
-      _value += command;
+      _setDigit(command);
     }
-  
+  }
+
+  _setOperation(String newOperation){
+    
+  }
+
+  _setDigit(String digit){
+    _value += digit;
   }
 
   allClear(){
