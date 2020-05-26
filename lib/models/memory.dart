1f3990c3491f720d9ed20ba80e89e35f88acf341
Alisson Goulart <alissonbrenoemp@gmail.com>
2020-05-26 10:22:48 -0300

    lógicas dos digitos númericos da memória

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class Memory{
   static const operations = const ['%', '/', 'X', '-', '+', '='];
   
@@ -5,6 +7,7 @@ class Memory{
   int _bufferIndex = 0;
   String operation;
   String _value = '0';
+  bool _wipeValue = false;
 
   void applyCommand(String command){
     if(command == 'AC'){
@@ -12,16 +15,29 @@ class Memory{
     } else if(operations.contains(command)){
       _setOperation(command);
     } else{
-      _setDigit(command);
+      _addDigit(command);
     }
   }
 
   _setOperation(String newOperation){
-    
+    _wipeValue = true;
   }
 
-  _setDigit(String digit){
-    _value += digit;
+  _addDigit(String digit){
+    final isDot = digit == '.';
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+    if(isDot && _value.contains('.') && !wipeValue){
+      return;
+    }
+
+    final emptyValue = isDot ? '0' : '';
+    final currentValue = wipeValue ? emptyValue : _value;
+    _value = currentValue + digit;
+    _wipeValue = false;
+    
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? '0';
+    //print(_buffer[_bufferIndex]);
   }
 
   allClear(){
